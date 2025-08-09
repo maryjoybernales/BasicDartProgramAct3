8ceb3b9b2b01cde516ca60c92da1c964f5c834a1
Mary Joy A. Bernales <ms.maryjoybernales@gmail.com>
2025-08-10 01:09:08 +0800

    Dart simple calculator program.

--- Calculator.dart
+++ Calculator.dart
@@ -1,25 +1,19 @@
-  void main() {
+void main() {
+  print("Simple Calculator");
   double num1 = 15;
   double num2 = 550;
 
-  
-  double mul = num1 * num2;
-  double diff = num1 - num2;
-  double sum = num1 + num2;
+  print("First number: $num1");
+  print("Second number: $num2\n");
 
-  print("Simple Calculator");
-  print("First Num: $num1");
-  print("Second Num: $num2");
-  print("");
-
-  print("Multiplication : $num1 * $num2 = $mul");
-  print("Subtraction    : $num1 - $num2 = $diff");
-  print("Addition       : $num1 + $num2 = $sum");
+  print("Multiplication: ${num1 * num2}");
+  print("Subtraction: ${num1 - num2}");
+  print("Addition: ${num1 + num2}");
 
   if (num2 == 0) {
-  print("Division       : Error! Division by zero is not allowed.");
-} else {
-  double div = num1 / num2;
-  print("Division       : $num1 / $num2 = $div");
+    print("Cannot divide by zero.");
+  } else {
+    print("Division: ${num1 / num2}");
   }
 }
+
